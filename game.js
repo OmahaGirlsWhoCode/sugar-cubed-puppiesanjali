781e14f1bc88d162b8ab48cb5957a90b3186f974
Anjali Jayan <anjalijayan415@gmail.com>
2020-03-22 18:18:08 -0500

    directions and enter key

--- game.js
+++ game.js
@@ -31,6 +31,7 @@ function create() {
     player1 = toybox.add.alien(playerOptions);
     
     playerScore = toybox.add.text(5,5,"SCORE: " + player1.score,{ font: "14px Arial", fill: "#fff"});
+    playerLives = toybox.add.text(5,20,"NUMBER OF LIVES: " + player1.health,{ font: "14px Arial", fill: "#fff"});
     game.time.events.loop(2000, createEnemies,this);
     
     createEnemies();
@@ -238,6 +239,35 @@ function update(){
     toybox.update();
     
     playerScore.setText("SCORE: "+ player1.score);
+    playerLives.setText("NUMBER OF LIVES: "+ player1.health);
+
+    if (player1.health === 0 && game.paused === false) {
+        game.paused = true;
+        playerGameover = toybox.add.text(9,200,"GAME OVER",{ font: "100px Arial", fill: "black"});
+        playerRestart = toybox.add.text(85,300,"Press D for directions or Enter to restart the game",{ font: "20px Arial", fill: "white" })
+        letterD = game.input.keyboard.addKey(68)
+        letterEnter = game.input.keyboard.addKey(13)
+        letterD.onDown.add(displaydirections, this);
+        letterEnter.onDown.add(restart, this);
+    }
+}
+
+var playerGameover;
+var playerRestart;
+
+function displaydirections () {
+    playerGameover.destroy ()
+    playerRestart.destroy ()
+    displaydirectionstext = toybox.add.text(85,300,"Your goal is to kill as many of those slippery slimes as possible! In order to do so, jump on top of them using the space bar. But be careful. You only have three lives.",{ font: "20px Arial", fill: "black", wordWrap: true, wordWrapWidth: 500 })
+}
+
+function restart () {
+    toybox.clear()
+    preload ()
+    create ()
+    game.paused = false;
+    game.input.keyboard.removeKey(68)
+    game.input.keyboard.removeKey(13)
 }
 
 function createEnemies(){
